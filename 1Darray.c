40b09f9f260f8fd81783be0eba17b7bd71c2e9d4
drewfowler <drewfowler17@gmail.com>
2018-06-27 08:17:35 -0600

    1Darray Challenge

--- 1Darray.c
+++ 1Darray.c
@@ -6,5 +6,22 @@
 int main() {
 
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int arlen;
+    int sum = 0;
+
+    scanf("%d", &arlen);
+
+    int arr[arlen];
+
+    for (size_t i = 0; i < arlen; i++) {
+      scanf("%d", &arr[i]);
+    }
+
+    for (size_t i = 0; i < arlen; i++) {
+      sum += arr[i];
+    }
+
+    printf("%d\n",sum );
+
     return 0;
 }
